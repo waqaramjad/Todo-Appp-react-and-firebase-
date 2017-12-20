f1578b1148b86776376208256ab6f988da9fa504
waqaramjad <waqaramjad345@gmail.com>
2017-12-20 08:45:18 +0500

    pplication startung

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Counterp from './CounterP'
+import Forms from './forms'
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<Counterp/>, document.getElementById('root'));
+ReactDOM.render(<Forms/>, document.getElementById('root'));
+// registerServiceWorker();
