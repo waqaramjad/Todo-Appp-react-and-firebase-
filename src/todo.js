5835438c97c6456a8186da38828aabb40a325d18
waqaramjad <waqaramjad345@gmail.com>
2017-12-21 14:56:19 +0500

    Deleted nodes done

--- src/todo.js
+++ src/todo.js
@@ -11,6 +11,8 @@ constructor(props)
        
     };
 
+    this.deleteHandler = this.deleteHandler.bind(this)
+
     firebase.database().ref('/').child('todos').on('child_added',(ev)=>
 {
 // console.log('ev',ev)
@@ -31,6 +33,39 @@ this.setState({
 })
 }
 
+deleteHandler(ev){
+
+//    console.log(ev.target.database)
+//    console.log(ev.target.value)
+//    console.log(ev)
+ let deletedNode = ev.target.value
+   firebase.database().ref('todos/'+deletedNode).remove()
+//    firebase.database().ref('todos/').on("child_removed",function(data){})
+
+.then((v) => {
+    // console.log(v)
+          let currentTodo = this.state.todos;
+          let indexRemove;
+          for (var i = 0; i < currentTodo.length; i++) {
+              if (currentTodo[i].id === deletedNode) {
+                  indexRemove = i;
+              }
+          }
+          currentTodo = currentTodo.slice(0, indexRemove).concat(currentTodo.slice(indexRemove + 1));
+          this.setState({ todos: currentTodo });
+      });
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
@@ -39,13 +74,16 @@ render(){
     return(
 <div>
 {this.state.todos.map((values, numbers) =>{
-    console.log('values', values)
+    // console.log('values', values)
+    // console.log('keys', values.id)
+    // console.log('numbers.toString()', numbers)
+
     // console.log('values', numbers)
     return(
         <div>
 
 {/* <h1 key={values.key}>{values.todosirug}</h1> */}
-<h1 key={numbers.toString()}>{values.todos} </h1>
+<h1 key={numbers}>{values.todos}<span><button value={values.id} onClick={this.deleteHandler} >Delete</button><button value={values.id} onClick={this.deleteHandler} >Delete</button></span></h1>
 
         </div>
     )
